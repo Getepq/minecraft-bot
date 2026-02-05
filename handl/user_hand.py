23154ee26e0469895e0662b4bfe8422bf5c667f8
Timye <114354569+Getepq@users.noreply.github.com>
2026-02-06 00:42:20 +0500

    Update user_hand.py

--- handl/user_hand.py
+++ handl/user_hand.py
@@ -3,7 +3,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery, LabeledPrice
 from aiogram.types import PreCheckoutQuery
 import handl.keyboard as kb
-from db.dbMOD import get_modpack
+from db.dbmod import get_modpack
 
 rt = Router()
 
