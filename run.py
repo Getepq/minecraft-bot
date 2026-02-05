31c70ecee5b10c9ecb7402d7e0b2512d1a00d907
Timye <114354569+Getepq@users.noreply.github.com>
2026-02-06 00:04:19 +0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from aiogram import Bot ,Dispatcher
+from handl.user_hand import rt as usr_hand
+from handl.admin_hand import rt as admin_hand
+from db.dbMod import create_db
+import os
+
+bot = Bot(token=os.getenv("TOKEN"))
+dp = Dispatcher()
+
+dp.include_router(usr_hand)
+dp.include_router(admin_hand)
+
+async def main():
+    await create_db()
+    print('DB created')
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Goodbye')
+
+
+
+
+
+
+
