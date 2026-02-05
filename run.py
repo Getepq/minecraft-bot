e131f4e2c0438f0279711e4b88fc092814c72f17
Timye <114354569+Getepq@users.noreply.github.com>
2026-02-06 00:35:59 +0500

    Update run.py

--- run.py
+++ run.py
@@ -19,9 +19,11 @@ async def main():
 
 if __name__ == '__main__':
     try:
+        logging.basicConfig(level=logging.DEBUG)
         asyncio.run(main())
     except KeyboardInterrupt:
         print('Goodbye')
+
 
 
 
